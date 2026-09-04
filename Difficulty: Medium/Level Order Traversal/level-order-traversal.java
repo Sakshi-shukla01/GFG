2c0736ceda014b122a7ace95bedde698edb54807
Sakshi shukla <sakshishukla1008@gmail.com>
2026-09-04 08:10:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Level Order Traversal/level-order-traversal.java
@@ -0,0 +1,40 @@
+/* Structure of Binary Tree Node
+class Node {
+    public int data;
+    public Node left;
+    public Node right;
+
+    // Constructor
+    public Node(int val) {
+        data = val;
+        left = right = null;
+    }
+};*/
+
+class Solution {
+    public ArrayList<Integer> levelOrder(Node root) {
+        // code here
+        ArrayList<Integer> ans=new ArrayList<>();
+        if(root==null){
+            return ans;
+        }
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        
+        while(!q.isEmpty()){
+            Node curr=q.poll();
+            ans.add(curr.data);
+            
+            if(curr.left!=null){
+                q.add(curr.left);
+            }
+            if(curr.right!=null){
+                
+                q.add(curr.right);
+            }
+        }
+        return ans;
+        
+        
+    }
+}
